2d3356e2bc8ce5da564c86a191e90ff2ff2d75fa
Felipe Sander <felipe.panisson@hotmail.com>
2022-12-14 14:46:50 -0300

    prisma service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,28 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
-
+import { Body, Post } from '@nestjs/common/decorators';
+import { randomUUID } from 'crypto';
+import { CreateNotificationBody } from './create-notification-body';
+import { PrismaService } from './prisma.service';
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly prisma: PrismaService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  list() {
+    return this.prisma.notification.findMany();
+  }
+
+  @Post()
+  async create(@Body() body: CreateNotificationBody) {
+    const { recipientId, content, category } = body;
+
+    await this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        content,
+        category,
+        recipientId,
+      },
+    });
   }
 }
