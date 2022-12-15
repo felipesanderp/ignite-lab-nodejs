a8a80fb6dde70939a3d2c89e5fa7e209ba04aa0c
Felipe Sander <felipe.panisson@hotmail.com>
2022-12-15 16:32:13 -0300

    implemnets methods

--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -4,9 +4,18 @@ import { Notification } from '@application/entities/notification';
 export class InMemoryNotificationsRepository
   // eslint-disable-next-line prettier/prettier
   implements NotificationsRepository {
+
   public notifications: Notification[] = [];
+
+  async findById(notificationId: string): Promise<Notification | null> {
+    throw new Error('Method not implemented.');
+  }
 
   async create(notification: Notification) {
     this.notifications.push(notification);
+  }
+
+  async save(notification: Notification): Promise<void> {
+    throw new Error('Method not implemented.');
   }
 }
