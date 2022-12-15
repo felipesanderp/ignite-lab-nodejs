5f041ce20e5abac3b594f47cdfca24725716b507
Felipe Sander <felipe.panisson@hotmail.com>
2022-12-15 09:04:00 -0300

    inject class

--- src/infra/http/controllers/notifications.controller.ts
+++ src/infra/http/controllers/notifications.controller.ts
@@ -1,11 +1,22 @@
 import { Controller } from '@nestjs/common';
 import { Body, Post } from '@nestjs/common/decorators';
+import { SendNotification } from '../../../application/use-cases/send-notification';
 import { CreateNotificationBody } from '../dtos/create-notification-body';
 
 @Controller()
 export class NotificationsController {
+  constructor(private sendNotifications: SendNotification) {}
+
   @Post()
   async create(@Body() body: CreateNotificationBody) {
     const { recipientId, content, category } = body;
+
+    const { notification } = await this.sendNotifications.execute({
+      recipientId,
+      content,
+      category,
+    });
+
+    return { notification };
   }
 }
