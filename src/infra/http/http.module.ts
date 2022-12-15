83188d2e229ef64b97612a3d8abb769065f753ce
Felipe Sander <felipe.panisson@hotmail.com>
2022-12-15 09:03:41 -0300

    make class injectable

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { SendNotification } from '../../application/use-cases/send-notification';
 import { NotificationsController } from './controllers/notifications.controller';
 
 @Module({
   imports: [],
   controllers: [NotificationsController],
+  providers: [SendNotification],
 })
 export class HttpModule {}
