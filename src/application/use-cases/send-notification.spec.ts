9faf284e0847bbef4bb8a00a0e8d828421cc170c
felipesanderp <felipe.panisson@hotmail.com>
2022-12-15 00:01:04 -0300

    in memory repository for tests

--- src/application/use-cases/send-notification.spec.ts
+++ src/application/use-cases/send-notification.spec.ts
@@ -1,8 +1,10 @@
 import { SendNotification } from './send-notification';
+import { InMemoryNotificationsRepository } from '../../../test/repositories/in-memory-notifications-repository';
 
 describe('Send Notification', () => {
   it('should be able to send a notification', async () => {
-    const sendNotification = new SendNotification();
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const sendNotification = new SendNotification(notificationsRepository);
 
     const { notification } = await sendNotification.execute({
       content: 'This is a notification',
@@ -10,6 +12,7 @@ describe('Send Notification', () => {
       recipientId: 'example-recipient-id',
     });
 
-    expect(notification).toBeTruthy();
+    expect(notificationsRepository.notifications).toHaveLength(1);
+    expect(notificationsRepository.notifications[0]).toEqual(notification);
   });
 });
